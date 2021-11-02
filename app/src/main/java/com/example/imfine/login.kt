0cf204c6c77ae0a94b1026585d8ead5e7a2104a6
Ji Seok <38849158+JiSeokYeom@users.noreply.github.com>
2021-11-03 00:12:04 +0900

    test

---
+++ app/src/main/java/com/example/imfine/login.kt
@@ -0,0 +1,13 @@
+package com.example.imfine
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class login :AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+
+    }
+
+}
